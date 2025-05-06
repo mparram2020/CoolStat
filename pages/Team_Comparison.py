7420bd5e2b7b6234a68e94e8aaffeb0f0f5d826a
mparram2020 <marcos.parra@estudiante.uam.es>
2025-05-06 18:02:46 +0200

    Empezando dashboards de Tableau

--- pages/Team_Comparison.py
+++ pages/Team_Comparison.py
@@ -1,25 +1,49 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-import numpy as np
 import streamlit.components.v1 as components
 
 # Configuración de la página
-st.set_page_config(page_title="Ranking", page_icon="logo.png", layout="wide")
+st.set_page_config(page_title="Team Comparison", page_icon="logo.png", layout="wide")
 
 st.title("Dashboard de estadísticas")
 
-# No coger directamente la URL de Tableau, sino la URL de la vista pública
-tableau_url = "https://public.tableau.com/views/Goles-xG/Dashboard1?:embed=true&:display_count=yes&:showVizHome=no"
+# Selección de competición
+competition = st.radio("Competición", ["Eurocopa", "Copa América"], index=0)
 
-# Selector de estadísticas generales de las competiciones
-selected_stat = st.selectbox("", ("Liga Española", "Premier League", "Serie A", "Bundesliga", "Ligue 1"), index=0)
+# Selector de estadísticas
+selected_stat = st.selectbox("Selecciona una estadística", ["Goals-xG per 90'", "Top Scorers", "Most Asists", "Bundesliga", "Ligue 1"], index=0)
 
-if selected_stat == "Liga Española":
-    st.write("En esta sección se muestran las estadísticas de goles y xG de los jugadores de la liga española.")
-    st.write("Para más información, visita el dashboard interactivo a continuación:")
-    # Embed Tableau dashboard
+# Mostrar iframe solo si coincide
+if selected_stat == "Goals-xG per 90'":
+    if competition == "Eurocopa":
+        components.iframe(
+            "https://public.tableau.com/views/Book2_17464506741000/Dashboard1?:embed=true&:display_count=yes&:showVizHome=no",
+            width=1800,
+            height=600,
+            scrolling=True
+        )
+    elif competition == "Copa América":
+        components.iframe(
+            "https://public.tableau.com/views/Book3_17464790157420/ScatterPlotGoalsxGnormalizedper90?:embed=true&:display_count=yes&:showVizHome=no",
+            width=1800,
+            height=600,
+            scrolling=True
+        )
 
-    components.iframe(tableau_url, width=1800, height=600, scrolling=True)
+st.info("Este dashboard muestra las estadísticas de goles y xG normalizadas por 90 minutos.")
 
-st.write("Este dashboard muestra las estadísticas de goles y xG de los jugadores de la liga española.")
+"""elif selected_stat == "Top Scorers":
+    if competition == "Eurocopa":
+        components.iframe(
+            "",
+            width=1800,
+            height=600,
+            scrolling=True
+        )
+    elif competition == "Copa América":
+        components.iframe(
+            "",
+            width=1800,
+            height=600,
+            scrolling=True
+        )
+    st.info("Este dashboard muestra los máximos goleadores de la competición seleccionada.")"""
